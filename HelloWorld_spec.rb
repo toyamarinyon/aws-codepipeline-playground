27c5df3e0dd421246166d09640d79e6e3c0cc3e3
toyama satoshi <toyamarinyon@gmail.com>
2018-11-07 13:18:22 +0900

    Fail test

--- HelloWorld_spec.rb
+++ HelloWorld_spec.rb
@@ -5,7 +5,7 @@ describe HelloWorld do
     it "The say_hello method should return 'Hello World'" do
       hw = HelloWorld.new
       message = hw.say_hello
-      puts 'Succeed' if expect(message).to eq "Hello, world!"
+      puts 'Succeed' if expect(message).to eq "Hello, new world!"
     end
   end
 end
